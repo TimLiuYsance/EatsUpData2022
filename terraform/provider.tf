410084db3d11b21687f8a41aa7413f3b4acde932
Tim LIU <tim.liu@devoteam.com>
2022-04-08 00:58:36 +0200

    cloudbuild test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,12 @@ provider "google" {
   project = var.project
   region  = var.region
 }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.57.0"
+    }
+  }
+}
