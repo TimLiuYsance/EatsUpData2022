d2c8229c94acd7a0a18df37f22b8fa2696d83ac3
Tim LIU <tim.liu@devoteam.com>
2022-04-08 00:29:48 +0200

    added cloudbuild

--- src/test.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import storage
-
-def upload_file_to_gcs():
-    print(storage.Client().get_bucket('ysance-eatsupdata-1-test_bucket').blob('job_test_copy_2.py').upload_from_filename('/Users/tliu/Documents/office/projects/ysance/EatsUpData2022/src/jobs/job_test_copy_2.py'))
-
-upload_file_to_gcs()
